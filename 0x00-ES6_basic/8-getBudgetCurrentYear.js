d2941ae291144ce83f87385c08f30919c9356f06
GammachisT <gammachistesfaye@gmail.com>
2024-09-30 16:14:16 +0000

    ES6 basics

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,5 +1,10 @@
 import getBudgetObject from './7-getBudgetObject';
 
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
