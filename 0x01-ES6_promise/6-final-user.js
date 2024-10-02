1ed4ad144ebb13afa3079f5893a32353c2eb6cd7
GammachisT <gammachistesfaye@gmail.com>
2024-10-02 07:15:32 +0000

    Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,7 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = await signUpUser(firstName, lastName).then((data) => ({
-    status: 'fulfilled',
-    value: data,
-  }));
-
-  const UserPhotoUpload = await uploadPhoto(fileName).catch((err) => ({
-    status: 'rejected',
-    value: err.toString(),
-  }));
-
-  return Promise.resolve([userPromise, userPhotoUpload]);
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
 }
