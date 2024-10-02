88859c24e748842672adf02b7ba27bc1ca4ca9d1
GammachisT <gammachistesfaye@gmail.com>
2024-10-02 06:46:53 +0000

    Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,12 @@
 export default function handleResponseFromAPI(promise) {
     return promise
         .then(() => {
-            console.log("Got a response from the API");
             return {
                 status: 200,
                 body: 'success'
             };
         })
-        .catch(() => {
-            console.log("Got a response from the API");
-            return new Error(); // Return an empty Error object
-        });
+        .catch(() => new Error())
+	.finally(() => console.log("Got a response from the API");
+        );
 }
